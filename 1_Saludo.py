8c1f18f10b5deaf8ad93cc2f83f73d0664f2d3d0
MiguelAngelCasillasAraiza <miguel.casillas@gmail.com>
2023-01-18 11:13:32 -0600

    Ver1

---
+++ 1_Saludo.py
@@ -0,0 +1,38 @@
+"""Saludo.py
+TNM/ITL
+9/Enero/2023
+Diplomado: 
+Automatización del despliegue de aplicaciones (DevOps) : 
+"Linux y Python para DevOps"
+por: Cirino Silva Tovar
+"""
+
+from datetime import datetime
+
+def meta():
+    print("Para saludar\n")
+
+def data():
+    global timeObj
+    
+    timeObj = datetime.now()
+    print("Hoy es ", end="")
+    print(timeObj)
+    
+def resultado():
+    """saludo = "Buenos Dias" if timeObj.hour < 12 else "Buenas Tardes" \
+                       if timeObj.hour < 19 else "Buenas Noches"
+    """
+    if timeObj.hour < 12:
+       saludo = "BuenosDias"
+    elif timeObj.hour < 19:
+       saludo = "Buenas Tardes"
+    else:
+       saludo = "Buenas Noches"
+    print (saludo)
+ 
+meta()   
+data()
+resultado()
+ 
+ 
